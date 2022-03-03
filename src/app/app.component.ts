59d7e0f7a1507428028a828bf37b4a07ba3c8d60
Shameem <shameemakhtar111@gmail.com>
2022-03-03 10:13:06 +0530

    angular todo first app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-todo';
+  constructor(){
+    // setTimeout(() => {
+    //   this.title = "Changed Title";
+    // }, 2000);
+  }
+  
 }
